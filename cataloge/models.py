9aa43a9a9b6ace5aa4fcebd570298323cd6c41bf
Yosefash <youssefashraf11y@gmail.com>
2025-04-16 12:33:40 +0200

    first commit

---
+++ cataloge/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Genere(models.Model):
+    name=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Book(models.Model):
+    title=models.CharField(max_length=50)
+    author=models.ForeignKey('Author',on_delete=models.SET_NULL,null=True)
+    summary=models.TextField()
+    isbin=models.CharField('ISBIN',max_length=50,unique=True)
+    genre=models.ManyToManyField(Genere)
+    language=models.ForeignKey("Language",on_delete=models.SET_NULL,null=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("book_detail", kwargs={"pk": self.pk})
+    
+class Author(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    date_of_birth=models.DateField(blank=True,null=True)
+    
+    class meta:
+        ordering=['last_name','first_name']
+    
+    def __str__(self):
+        return f'{self.last_name}-{self.first_name}'
+    
+    def get_absolute_url(self):
+        return reverse("author_detail", kwargs={"pk": self.pk})
+    
+    
+# ------------------------
+import uuid
+
+class BookInstance(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4)
